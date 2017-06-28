d5e266a932c1a0d2685f96194b625f85c3aa1e77
Savk <savksilva@gmail.com>
2017-06-27 22:27:47 -0400

    initial commit

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,12 +1,66 @@
 import React from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
 import { Images } from '../Themes'
-
+import BackgroundJob from 'react-native-background-job';
+import Firestack from 'react-native-firestack'
+const firestack = new Firestack();
+var SmsAndroid = require('react-native-sms-android');
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends React.Component {
-  render () {
+  componentDidMount() {
+    
+    console.log("LaunchScreen");
+    firestack.database.ref('msg').once('value')
+      .then((snapshot) => {
+        const val = snapshot.val();
+        console.log("firebase Data:", val);
+      }).catch((err) => {
+        console.log('An error occurred', err);
+      })
+    const backgroundJob = {
+      jobKey: "myJob",
+      job: () => {
+        console.log("Running in background")
+        firestack.database.ref('msg').once('value')
+          .then((snapshot) => {
+            const val = snapshot.val();
+            console.log("firebase Data:", val);
+          })
+        firestack.database.ref('msg').on('value', (snapshot) => {
+          const val = snapshot.val();
+          SmsAndroid.sms(
+            '04167787357', // phone number to send sms to
+            val, // sms body
+            'sendDirect', // sendDirect or sendIndirect
+            (err, message) => {
+              if (err) {
+                console.log("error");
+              } else {
+                console.log(message); // callback message
+              }
+            }
+          );
+          console.log("firebase Data update:", val);
+        });
+      }
+    };
+
+    BackgroundJob.register(backgroundJob);
+    var backgroundSchedule = {
+      jobKey: "myJob",
+      timeout: 5000,
+      // period: 5000,
+      // alwaysRunning: true,
+      notificationTitle: "test",
+      networkType: BackgroundJob.NETWORK_TYPE_METERED
+    }
+    BackgroundJob.cancelAll();
+    //BackgroundJob.schedule(backgroundSchedule);
+    BackgroundJob.getAll({ callback: console.log });
+  }
+  render() {
     return (
       <View style={styles.mainContainer}>
         <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
--- android/app/src/main/java/com/kawii/MainApplication.java
+++ android/app/src/main/java/com/kawii/MainApplication.java
@@ -3,6 +3,9 @@ package com.kawii;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rhaker.reactnativesmsandroid.RNSmsAndroidPackage;
+import com.pilloxa.backgroundjob.BackgroundJobPackage;
+import io.fullstack.firestack.FirestackPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNSmsAndroidPackage(),
+            new BackgroundJobPackage(),
+            new FirestackPackage()
       );
     }
   };
